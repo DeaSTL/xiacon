ef939e7fc853417f3a5732b26e11a6021d9a879e
Kaleb Klein <klein.jae@gmail.com>
2017-04-24 15:57:11 -0400

    changes ;)
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- js/function_lib.js
+++ js/function_lib.js
@@ -42,7 +42,12 @@ function search_database(query) {
 
                 for (var i = 0; i < data.result.length; i++) {
                     var tmp = $("#search_output").html();
-                    $("#search_output").html(tmp + data.result[i] + "<br>");
+                    // $("#search_output").html(tmp + data.result[i] + "<br>");
+                    $("#search_output").html(tmp + '<a href="definition.php?q='
+                        + data.result[i]
+                        + '">'
+                        + data.result[i]
+                        + '</a><br>');
                 }
             }
         },
