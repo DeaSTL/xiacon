28a42ada1960083da940ce5ac79bc80d101b4b03
Kaleb Klein <klein.jae@gmail.com>
2017-04-24 15:33:09 -0400

    Made some changes
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- js/function_lib.js
+++ js/function_lib.js
@@ -4,22 +4,24 @@ function get_page_json(filename){
 	xm.send(null);
 	return xm.responseText;
 }
-function search_database(query){
-	$.ajax({
+
+function search_database(query) {
+    $.ajax({
         url: 'search.php',
         type: 'get',
         dataType: 'json',
         data: { q: query },
         success: function(data, status, xhr) {
-          
-            if(data.length == 0){
-                document.getElementById("search_output").innerHTML = "";
-            }else{
-                for(var i = 0;i<data.length;i++){
-                document.getElementById("search_output").innerHTML += data[i] + "</br>";
-            }    
+            if (data.error.result === true) {
+                console.log(data.error.message);
+                $("#search_output").html(data.error.message);
+            } else {
+
+                for (var i = 0; i < data.result.length; i++) {
+                    var tmp = $("#search_output").html();
+                    $("#search_output").html(tmp + data.result[i] + "<br>");
+                }
             }
-            
         },
         error: function(xhr, status, error) {
             console.log("Status: " + status + " // Error: " + error);
