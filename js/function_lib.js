e10548658c3fc62b7208ada155a5d6cc0c8df6ac
DeaSTL <jojoisawsome39@gmail.com>
2017-04-24 00:26:18 -0400

    Fixed the search feature

--- js/function_lib.js
+++ js/function_lib.js
@@ -1,13 +1,20 @@
 function get_page_json(filename){
-	var xhttp = new XMLHttpRequest();
-	var response = "";
-	xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	       response = xhttp.responseText;
-	    }
-	};
-	xhttp.open("GET", filename, true);
-	xhttp.send();
-	return response;
-
+	xm = new XMLHttpRequest();
+	xm.open("GET",filename,true);
+	xm.send(null);
+	return xm.responseText;
+}
+function search_database(query){
+	$.ajax({
+        url: 'search.php',
+        type: 'get',
+        dataType: 'json',
+        data: { q: query },
+        success: function(data, status, xhr) {
+            console.log(data);
+        },
+        error: function(xhr, status, error) {
+            console.log("Status: " + status + " // Error: " + error);
+        }
+    });
 }
---
+++ search.php
@@ -0,0 +1,11 @@
+<?php 
+	$link = new mysqli()
+
+	function search(){
+
+
+	}
+	if(isset($_GET['q'])){
+		echo json_encode($_GET);
+	}
+?>
