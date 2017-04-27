731da3798306b9229d3e91d98441191c8d5d3b64
DeaSTL <jojoisawsome39@gmail.com>
2017-04-26 21:23:45 -0400

    fixed some shit

--- search.php
+++ search.php
@@ -22,8 +22,8 @@ $return = [
 ];
 
 if (isset($_GET['q'])) {
-    $query = escape($link, $_GET['q']);
-    $q = $link->query("SELECT `word` FROM `entries` WHERE `word` LIKE '$query%' LIMIT 50");
+    $query = dictionary::escape($link, $_GET['q']);
+    $q = $dictionary_link->query("SELECT `word` FROM `entries` WHERE `word` LIKE '$query%' LIMIT 50");
     $return['error']['result'] = false;
     $return['result'] = $q->fetch_all();
 } else {
