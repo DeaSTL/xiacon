2da52dd10028e48cd55ea8a998d9dca7af46667a
Kaleb Klein <klein.jae@gmail.com>
2017-04-27 00:45:06 -0400

    Cleared out cache
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- display/cache/ab122175024d452ff378064de2801c4befdbdc4e.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-
-<div id="search_output"></div>
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layout.main', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
