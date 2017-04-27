7d5771f038d0a7fb2108b4334a2da1b664aae063
DeaSTL <jojoisawsome39@gmail.com>
2017-04-26 22:33:04 -0400

    added the interfaces dir

---
+++ assets/interface/navbar.php
@@ -0,0 +1,30 @@
+<body>
+    <nav class="navbar navbar-custom">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nbcol" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?=link_to('/')?>">Xiacon</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="nbcol">
+                <ul class="nav navbar-nav">
+                    
+                </ul>
+                <form action="#" class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search" id="search_bar">
+                    </div>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div id="search_output"></div>
+</body>
